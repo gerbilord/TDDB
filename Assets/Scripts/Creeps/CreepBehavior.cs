a898fff0d2e7c39873cc393bd8713227df46c3dc
Alessandro Cois <cois.alessandro@gmail.com>
2023-06-22 23:55:51 -0700

    Make creeps clickable.

--- Assets/Scripts/Creeps/CreepBehavior.cs
+++ Assets/Scripts/Creeps/CreepBehavior.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,6 +8,22 @@ public class CreepBehavior : MonoBehaviour, ICreep
 
     public Dictionary<StatType, float> stats { get; set; }
     public int currentPathIndex { get; set; }
+
+    private Animator _animator;
+
+    public void Awake()
+    {
+        // Get animator
+        _animator = GetComponent<Animator>();
+    }
+    
+    // If the creep was clicked
+    public void OnMouseDown()
+    {
+        // Change animator trigger "Clicked" to true. Turns back to false automatically after animation is done.
+        _animator.SetTrigger("Clicked");
+    }
+
     public GameObject GetGameObject()
     {
         return gameObject;
